2c0cdeb0c40abdc081be7d6b22108d323c003d95
kevinrivasd <82895654+kevinrivasd@users.noreply.github.com>
2023-02-12 20:15:44 -0600

    Ya esta el sidevar

--- js/inicializar.js
+++ js/inicializar.js
@@ -1,3 +1,5 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
   });
+ 
