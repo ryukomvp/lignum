36f320aa40d4642d821f6b002514e90812eed932
Daniel Hernández <daniel123hernandez15@gmail.com>
2023-02-22 09:34:17 -0600

    Página main definitiva

---
+++ js/main.js
@@ -0,0 +1,19 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+
+  var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  })
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems);
+  });
