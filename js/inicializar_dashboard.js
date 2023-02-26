bf980b81aaa483164e3ac469f44e795e57838f2d
kevinrivasd <82895654+kevinrivasd@users.noreply.github.com>
2023-02-25 21:24:50 -0600

    Casi terminado el diseño de productos

--- js/inicializar_dashboard.js
+++ js/inicializar_dashboard.js
@@ -2,3 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
