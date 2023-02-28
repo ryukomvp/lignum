59ae67bb5a2e9fe2a51f319c20c849037c5407a9
kevinrivasd <82895654+kevinrivasd@users.noreply.github.com>
2023-02-28 09:44:27 -0600

    Comentarios

--- recursos/js/inicializar_dashboard.js
+++ recursos/js/inicializar_dashboard.js
@@ -1,15 +1,16 @@
+// inicializar Sidenav
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
 
-
+// Inicializar modal
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
   });
 
-
+// Inicializar Select
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
