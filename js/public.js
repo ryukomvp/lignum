e0404abde8d4a86dc4bf514cfdaee660ad824ee9
Daniel Hernández <daniel123hernandez15@gmail.com>
2023-02-27 20:31:03 -0600

    Cambios en interfaces

--- js/public.js
+++ js/public.js
@@ -17,3 +17,8 @@
     var elems = document.querySelectorAll('.tooltipped');
     var instances = M.Tooltip.init(elems);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
