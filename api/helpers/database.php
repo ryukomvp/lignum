7375ac8c1c7c7c342eb457f63e9bc51baa2f275a
kevinrivasd <82895654+kevinrivasd@users.noreply.github.com>
2023-03-22 09:46:49 -0600

    Conexion a la base

---
+++ api/helpers/database.php
@@ -0,0 +1,76 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+require_once('config.php');
+
+/*
+*   Clase para realizar las operaciones en la base de datos.
+*/
+class Database
+{
+    // Propiedades de la clase para manejar las acciones respectivas.
+    private static $connection = null;
+    private static $statement = null;
+    private static $error = null;
+
+    /*
+    *   Método para ejecutar las sentencias SQL.
+    *   Parámetros: $query (sentencia SQL) y $values (arreglo con los valores para la sentencia SQL).
+    *   Retorno: booleano (true si la sentencia se ejecuta satisfactoriamente o false en caso contrario).
+    */
+    public static function executeRow($query, $values)
+    {
+        try {
+            // Se crea la conexión mediante la clase PDO con el controlador de PostgreSQL.
+            self::$connection = new PDO('pgsql:host=' . SERVER . ';dbname=' . DATABASE . ';port=5432', USERNAME, PASSWORD);
+            // Se prepara la sentencia SQL.
+            self::$statement = self::$connection->prepare($query);
+            // Se ejecuta la sentencia preparada y se retorna el resultado.
+            return self::$statement->execute($values);
+        } catch (PDOException $error) {
+            // Se obtiene el código y el mensaje de la excepción para establecer un error personalizado.
+            self::setException($error->getCode(), $error->getMessage());
+            return false;
+        }
+    }
+    
+    /*
+    *   Método para establecer un mensaje de error personalizado al ocurrir una excepción.
+    *   Parámetros: $code (código del error) y $message (mensaje original del error).
+    *   Retorno: ninguno.
+    */
+    private static function setException($code, $message)
+    {
+        // Se asigna el mensaje del error original por si se necesita.
+        self::$error = $message . PHP_EOL;
+        // Se compara el código del error para establecer un error personalizado.
+        switch ($code) {
+            case '7':
+                self::$error = 'Existe un problema al conectar con el servidor';
+                break;
+            case '42703':
+                self::$error = 'Nombre de campo desconocido';
+                break;
+            case '23505':
+                self::$error = 'Violación de unicidad';
+                break;
+            case '42P01':
+                self::$error = 'Nombre de tabla desconocido';
+                break;
+            case '23503':
+                self::$error = 'Violación de llave foránea';
+                break;
+            default:
+                self::$error = 'Ocurrió un problema en la base de datos';
+        }
+    }
+
+    /*
+    *   Método para obtener un error personalizado cuando ocurre una excepción.
+    *   Parámetros: ninguno.
+    *   Retorno: error personalizado.
+    */
+    public static function getException()
+    {
+        return self::$error;
+    }
+}
