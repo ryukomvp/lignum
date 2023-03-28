5339581827f5a41c17ee944687b0cdc45fad8837
ryukomvp <daniel123hernandez15@gmail.com>
2023-03-28 11:43:45 -0600

    Cambios a la plantilla

--- controllers/public/account.js
+++ controllers/public/account.js
@@ -10,16 +10,16 @@ document.addEventListener('DOMContentLoaded', async () => {
         <div class="navbar-fixed">
             <nav>
                 <div class="nav-wrapper">
-                    <a href="index.html" class="brand-logo"><img src="/resources/img/2.png" alt=""></a>
+                    <a href="/views/public/index.html" class="brand-logo"><i class="material-icons">home</i></a>
                     <a href="#" data-target="menu-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
                         <li><a href="#" class="tooltipped" data-position="bottom" data-tooltip="Inicio de la página"><i
                                     class="material-icons">arrow_upward</i></a></li>
-                        <li><a href="catalogue.html" class="tooltipped" data-position="bottom"
+                        <li><a href="/views/public/catalogue.html" class="tooltipped" data-position="bottom"
                                 data-tooltip="Catalogo"><i class="material-icons">library_books</i></a></li>
-                        <li><a href="shopping_cart.html" class="tooltipped" data-position="bottom"
+                        <li><a href="/views/public/shopping_cart.html" class="tooltipped" data-position="bottom"
                                 data-tooltip="Carrito de compras"><i class="material-icons">shopping_cart</i></a></li>
-                        <li><a href="login.html" class="tooltipped" data-position="bottom"
+                        <li><a href="/views/public/login.html" class="tooltipped" data-position="bottom"
                                 data-tooltip="Perfil del usuario"><i class="material-icons">account_circle</i></a></li>
                     </ul>
                 </div>
@@ -27,25 +27,25 @@ document.addEventListener('DOMContentLoaded', async () => {
         </div>
         <!-- menu para dispositivos con pantallas pequeños -->
         <ul class="sidenav" id="menu-mobile">
-            <li><a href="catalogue.html">Catalogo</a></li>
-            <li><a href="shopping_cart.html">Carrito</a></li>
-            <li><a href="login.html">Cuenta</a></li>
+            <li><a href="/views/public/catalogue.html">Catalogo</a></li>
+            <li><a href="/views/public/shopping_cart.html">Carrito</a></li>
+            <li><a href="/views/public/login.html">Cuenta</a></li>
         </ul>
     `;
-    })
     FOOTER.innerHTML = `
         <div class="container">
-        <div class="row">
-            <div class="col l6 s12">
-                <h5 class="white-text">Expertos en muebles</h5>
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Expertos en muebles</h5>
+                </div>
             </div>
         </div>
-        </div>
         <div class=" footer-copyright">
-        <div class="container">
-            <a href="/views/public/about_us.html">Sobre Lignum</a>
-            <a class="grey-text text-lighten-4 right" href="https://www.instagram.com/dnlhernandez_"
-                target="_blank"><img src="https://img.icons8.com/material-outlined/24/FFFFFF/instagram-new--v1.png"/></a>
-        </div>
+            <div class="container">
+                <a href="/views/public/about_us.html">Sobre Lignum</a>
+                <a class="grey-text text-lighten-4 right" href="https://www.instagram.com/dnlhernandez_"
+                    target="_blank"><img src="https://img.icons8.com/material-outlined/24/FFFFFF/instagram-new--v1.png"/></a>
+            </div>
         </div>
     `;
+})
