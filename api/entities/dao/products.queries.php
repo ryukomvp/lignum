6fd354e4c123188c49bd1489de4b0bef7fe972f8
kevinrivasd <82895654+kevinrivasd@users.noreply.github.com>
2023-03-27 19:22:00 -0600

    Trabajando con los set y get de proudctos aun no terminado

---
+++ api/entities/dao/products.queries.php
@@ -0,0 +1,109 @@
+<?php
+require_once('../../helpers/database.php');
+/*
+*	Clase para manejar el acceso a datos de la entidad PRODUCTO.
+*/
+class ProductoQueries
+{
+    /*
+    *   Métodos para realizar las operaciones SCRUD (search, create, read, update, delete).
+    */
+
+    /*metodo para buscar registros*/
+    public function searchRows($value)
+    {
+        $sql = 'SELECT id_producto, nomre_producto, descripcion_producto, precio_producto, codigo_producto, dimensiones, id_categoria, id_tipo_material, id_proveedor, id_estado_producto, cantidad_existencias
+                FROM producto INNER JOIN categorias USING(id_categoria)
+                WHERE nombre_producto ILIKE ? OR descripcion_producto ILIKE ?
+                ORDER BY nombre_producto';
+        $params = array("%$value%", "%$value%");
+        return Database::getRows($sql, $params);
+    }
+
+    public function createRow()
+    {
+        $sql = 'INSERT INTO productos(nomre_producto, descripcion_producto, precio_producto, codigo_producto, dimensiones, id_categoria, id_tipo_material, id_proveedor, id_estado_producto, cantidad_existencias, imagen_producto)
+                VALUES(?, ?, ?, ?, ?, ?, ?)';
+        $params = array($this->nombre, $this->descripcion, $this->precio, $this->codigo, $this->dimensiones, $this->categoria, $this->material, $this->proveedor, $this->estado, $this->imagen, $_SESSION['id_usuario_privado']);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function readAll()
+    {
+        $sql = 'SELECT id_producto, imagen_producto, nombre_producto, descripcion_producto, precio_producto, nombre_categoria, estado_producto
+                FROM productos INNER JOIN categorias USING(id_categoria)
+                ORDER BY nombre_producto';
+        return Database::getRows($sql);
+    }
+
+    public function readOne()
+    {
+        $sql = 'SELECT id_producto, nombre_producto, descripcion_producto, precio_producto, imagen_producto, id_categoria, estado_producto
+                FROM productos
+                WHERE id_producto = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    public function updateRow($current_image)
+    {
+        // Se verifica si existe una nueva imagen para borrar la actual, de lo contrario se mantiene la actual.
+        ($this->imagen) ? Validator::deleteFile($this->getRuta(), $current_image) : $this->imagen = $current_image;
+
+        $sql = 'UPDATE productos
+                SET imagen_producto = ?, nombre_producto = ?, descripcion_producto = ?, precio_producto = ?, estado_producto = ?, id_categoria = ?
+                WHERE id_producto = ?';
+        $params = array($this->imagen, $this->nombre, $this->descripcion, $this->precio, $this->estado, $this->categoria, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function deleteRow()
+    {
+        $sql = 'DELETE FROM productos
+                WHERE id_producto = ?';
+        $params = array($this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function readProductosCategoria()
+    {
+        $sql = 'SELECT id_producto, imagen_producto, nombre_producto, descripcion_producto, precio_producto
+                FROM productos INNER JOIN categorias USING(id_categoria)
+                WHERE id_categoria = ? AND estado_producto = true
+                ORDER BY nombre_producto';
+        $params = array($this->id);
+        return Database::getRows($sql, $params);
+    }
+
+    /*
+    *   Métodos para generar gráficas.
+    */
+    public function cantidadProductosCategoria()
+    {
+        $sql = 'SELECT nombre_categoria, COUNT(id_producto) cantidad
+                FROM productos INNER JOIN categorias USING(id_categoria)
+                GROUP BY nombre_categoria ORDER BY cantidad DESC';
+        return Database::getRows($sql);
+    }
+
+    public function porcentajeProductosCategoria()
+    {
+        $sql = 'SELECT nombre_categoria, ROUND((COUNT(id_producto) * 100.0 / (SELECT COUNT(id_producto) FROM productos)), 2) porcentaje
+                FROM productos INNER JOIN categorias USING(id_categoria)
+                GROUP BY nombre_categoria ORDER BY porcentaje DESC';
+        return Database::getRows($sql);
+    }
+
+    /*
+    *   Métodos para generar reportes.
+    */
+    public function productosCategoria()
+    {
+        $sql = 'SELECT nombre_producto, precio_producto, estado_producto
+                FROM productos INNER JOIN categorias USING(id_categoria)
+                WHERE id_categoria = ?
+                ORDER BY nombre_producto';
+        $params = array($this->categoria);
+        return Database::getRows($sql, $params);
+    }
+}
