a7a8787ff39335dd74b3d1b4bce0ede20e7feb14
AlecLarios2 <20200332@ricaldone.edu.sv>
2023-04-08 23:21:16 -0600

    asdfgh

--- api/entities/dao/pedidos_queries.php
+++ api/entities/dao/pedidos_queries.php
@@ -29,12 +29,53 @@ public function startOrder()
 
 public function createDetail()
 {
-    $sql = 'INSERT INTO detalle_pedido(id_pedido, id_prodcuto, precio_producto, cantidad, fecha) VALUES(?, (SELECT precio_producto FROM producto where id_producto = ?), ?, ?)';
-    $params = array($this->id_pedido, $this->producto, $this->producto, $this->id_pedido);
+    $sql = 'INSERT INTO detalle_pedido(id_producto, precio_producto, cantidad, id_pedido) VALUES(?, (SELECT precio_producto FROM producto where id_producto = ?), ?, ?)';
+    $params = array($this->producto, $this->producto, $this->cantidad, $this->id_pedido);
     return Database::executeRow($sql, $params)
 }
 
+//Metodo para obtener los productos en el carrito
+
+public function ReadOrderDetail()
+{
+    $sql = "SELECT id_detalle_pedido, nombre_producto, detalle_pedido.precio_producto, detalle_pedido.cantidad FROM pedido INNER JOIN detalle_pedido USING(id_pedido) INNER JOIN producto USING(id_producto) WHERE id_pedido = ?";
+    $params = array($this->id_pedido);
+    return Database::getRows($sql, $params);
 }
 
+//Metodo para terminar/cerrar un pedido del lado del cliente
+
+public function finishOrder()
+{
+    date_default_timezone_set('America/El_Salvador');
+        $date = date('Y-m-d');
+        $this->estado = 1;
+        $sql = 'UPDATE pedido
+                SET id_estado_pedido = ?, fecha = ?
+                WHERE id_pedido = ?';
+        $params = array($this->estado, $date, $_SESSION['id_pedido']);
+        return Database::executeRow($sql, $params);
+}
+
+// Método para actualizar la cantidad de un producto agregado al carrito de compras.
+public function updateDetail()
+{
+    $sql = 'UPDATE detalle_pedido
+            SET cantidad_producto = ?
+            WHERE id_detalle = ? AND id_pedido = ?';
+    $params = array($this->cantidad, $this->id_detalle, $_SESSION['id_pedido']);
+    return Database::executeRow($sql, $params);
+}
+
+// Método para eliminar un producto que se encuentra en el carrito de compras.
+public function deleteDetail()
+{
+    $sql = 'DELETE FROM detalle_pedido
+            WHERE id_detalle = ? AND id_pedido = ?';
+    $params = array($this->id_detalle, $_SESSION['id_pedido']);
+    return Database::executeRow($sql, $params);
+}
+
+}
 
 ?>
---
+++ api/entities/dto/pedidos.php
@@ -0,0 +1,99 @@
+<?php
+require_once('../../helpers/validator.php');
+require_once('../../entities/dao/pedidos_queries.php');
+
+//Clase para manejar transferencia de datos de pedido y detalle pedido
+
+class Pedidos extends PedidosQueries
+{
+    //Declaracion de atributos
+    protected $id_pedido = null;
+    protected $id_detalle_producto = null;
+    protected $cliente = null;
+    protected $producto = null;
+    protected $cantidad = null;
+    protected $precio = null;
+    protected $estado = null;
+
+
+    public function setIdPedido($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id_pedido = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setIdDetalle($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id_detalle_pedido = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setCliente($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->cliente = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setProducto($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->producto = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setCantidad($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->cantidad = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setPrecio($value)
+    {
+        if (Validator::validateMoney($value)) {
+            $this->precio = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setEstado($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->estado = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    *   Métodos para obtener valores de los atributos.
+    */
+    public function getIdPedido()
+    {
+        return $this->id_pedido;
+    }
+}
+}
+
+?>
