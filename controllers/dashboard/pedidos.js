674f1916895f4c046b13affff9bd9c9f376d95f8
AlecLarios2 <20200332@ricaldone.edu.sv>
2023-04-16 23:02:31 -0600

    asdfg

--- controllers/dashboard/pedidos.js
+++ controllers/dashboard/pedidos.js
@@ -1,9 +1,14 @@
 // Constante para completar la ruta de la API.
 const PEDIDO_API = 'business/public/pedido.php';
-// Constante para establecer el formulario de cambiar producto.
-const ITEM_FORM = document.getElementById('item-form');
-// Constante para establecer el cuerpo de la tabla.
+// Constante para establecer el formulario de buscar.
+const SEARCH_FORM = document.getElementById('search-form');
+// Constante para establecer el formulario de guardar.
+const SAVE_FORM = document.getElementById('save-form');
+// Constante para establecer el título de la modal.
+const MODAL_TITLE = document.getElementById('modal-title');
+// Constantes para establecer el contenido de la tabla.
 const TBODY_ROWS = document.getElementById('tbody-rows');
+const RECORDS = document.getElementById('records');
 // Constante tipo objeto para establecer las opciones del componente Modal.
 const OPTIONS = {
     dismissible: false
@@ -11,12 +16,12 @@ const OPTIONS = {
 // Se inicializa el componente Modal para que funcionen las cajas de diálogo.
 M.Modal.init(document.querySelectorAll('.modal'), OPTIONS);
 // Constante para establecer la caja de diálogo de cambiar producto.
-const ITEM_MODAL = M.Modal.getInstance(document.getElementById('item-modal'));
+const ITEM_MODAL = M.Modal.getInstance(document.getElementById('save-modal'));
 
 // Método manejador de eventos para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
     // Llamada a la función para mostrar los productos del carrito de compras.
-    readOrderDetail();
+    filltable();
 });
 
 // Método manejador de eventos para cuando se envía el formulario de cambiar cantidad de producto.
