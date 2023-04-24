1a0d94edb00b5de09ddd59d23c0a3488d4630296
AlecLarios2 <20200332@ricaldone.edu.sv>
2023-04-24 11:26:36 -0600

    asddfg

---
+++ api/entities/dao/estado_pedido_queries.php
@@ -0,0 +1,51 @@
+<?php
+require_once('../../helpers/database.php');
+
+Class EstadoPedidoQueries{
+
+    //Operaciones SCRUD
+
+    public function searchOrder($value)
+{
+    $sql = 'SELECT id_pedido, codigo_pedido, descripcion_pedido, nombre_cliente, estado_pedido FROM pedido INNER JOIN cliente USING(id_cliente) INNER JOIN estado_pedido USING(id_estado_pedido) WHERE codigo_pedido ILIKE ? or descripcion_pedido ILIKE ? ORDER BY id_producto';
+    $params = array("%$value%", "%$value%");
+    return Database::getRows($sql, $params);
+}
+
+
+public function createOrder()
+{
+    $sql = 'INSERT INTO pedido(codigo_pedido, descripcion_pedido, id_cliente, id_estado_pedido) VALUES(?, ?, (SELECT id_cliente FROM cliente WHERE id_cliente = ?), (SELECT id_estado_pedido FROM estado_pedido WHERE id_estado_pedido = ?))';
+    $params = array($this->codigo, $this->descripcion, $this->cliente, $this->estado);
+    return Database::executeRows($sql, $params);
+}
+
+public function readAll()
+{
+    $sql = 'SELECT id_pedido, codigo_pedido, descripcion_pedido, nombre_cliente, estado_pedido from pedido INNER JOIN cliente USING(id_cliente) INNER JOIN estado_pedido USING(id_estado_pedido) ORDER BY id_pedido';
+    return Database::getRows($sql);
+}
+
+public function readOne()
+{
+    $sql = 'SELECT id_pedido, codigo_pedido, descripcion_pedido, id_cliente, id_estado_pedido from pedido WHERE id_producto = ?';
+    $params = array($this->id_pedido);
+    return Database::getRows($sql, $params);
+}
+
+public function updateOrder()
+{
+    $sql = 'UPDATE pedido SET codigo_pedido = ?, descripcion_pedido = ?, id_cliente = ?, id_estado_pedido= ? WHERE id_pedido = ?';
+    $params = array($this->codigo, $this->descripcion, $this->cliente, $this->estado);
+    return Database::executeRows($sql, $params);
+}
+
+public function deleteOrder()
+{
+    $sql = 'DELETE FROM pedido WHERE id_pedido = ?';
+    $params = array($this->id_pedido);
+    return Database::executeRows($sql, $params);
+}
+}
+
+?>
--- controllers/dashboard/pedidos.js
+++ controllers/dashboard/pedidos.js
@@ -1,5 +1,6 @@
 // Constante para completar la ruta de la API.
 const PEDIDO_API = 'business/dashboard/pedido.php';
+const CLIENTE_API = 'business/dashboard/cliente.php';
 // Constante para establecer el formulario de buscar.
 const SEARCH_FORM = document.getElementById('search-form');
 // Constante para establecer el formulario de guardar.
