7154be3944352d9b73d41c0509f155a23958972c
kevinrivasd <82895654+kevinrivasd@users.noreply.github.com>
2023-05-02 14:04:13 -0600

    camvios para unir las ramas

--- api/helpers/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Constantes para establecer las credenciales de conexión con el servidor de bases de datos.
-define('SERVER', 'localhost');
-define('DATABASE', 'lignum');
-define('USERNAME', 'postgres');
-define('PASSWORD', '1234');
