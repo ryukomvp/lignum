70c9496fb5c54674f8a4bc8fa28df90b43b3f7be
AlecLarios2 <20210466@ricaldone.edu.sv>
2023-05-15 14:05:50 -0600

    asdasd

--- controllers/public/detail.js
+++ controllers/public/detail.js
@@ -9,4 +9,14 @@ const SHOPPING_FORM = document.getElementById('shopping_form');
 M.Tooltip.init(document.querySelectorAll('.tooltipped'));
 
 //Metodo manejador de eventos para cuando el documento ha cargado
-document.addEventListener('DOMContentLoaded', async)
+document.addEventListener('DOMContentLoaded', async () => {
+    //Constante tipo objeto con los datos del producto seleccionado
+    const FORM = new FormData();
+    FORM.append('id_producto', PARAMS.get('id'));
+    //Peticion para seleccionar los datos del producto seleccionado
+    const JSON = await dataFetch(PRODUCTO_API, 'readOne', FORM);
+    //Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepcion
+    if(JSON.status){
+        //Se colocan los datos en la pagina web de acuerdo con el producto seleccionado previamente
+    }
+}) 
