a9c3ef91dbcadf2e03c33fb064e46dd11b528e56
ryukomvp <daniel123hernandez15@gmail.com>
2023-05-22 07:41:22 -0600

    Avances catalogo

--- controllers/public/catalogue.js
+++ controllers/public/catalogue.js
@@ -6,43 +6,67 @@ const PARAMS = new URLSearchParams(location.search);
 const TITULO = document.getElementById('title');
 const PRODUCTOS = document.getElementById('productos');
 
-//Metodo manejador de eventos para cuando el documento ha cargado
-document.addEventListener('DOMContentLoaded', async () => {
-    //Se define un objeto con los datos de la categoria seleccionada
-    const FORM = new FormData();
-    FORM.append('id_categoria', PARAMS.get('id'));
-    //Peticion para solicitar los productos de la categoria seleccionada
-    const JSON = await dataFetch(PRODUCTO_API, 'readproductoCategoria', FORM);
-    if(JSON.status){
-        //Se inicializa el contenedor de productos
-        PRODUCTOS.innerHTML = '';
+// Método manejador de eventos para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para llenar la tabla con los registros disponibles.
+    cargarProductos();
+});
+
+// Método manejador de eventos para cuando se envía el formulario de buscar.
+SEARCH_FORM.addEventListener('submit', (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SEARCH_FORM);
+    // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+    cargarProductos(FORM);
+});
+
+async function cargarProductos(form = null) {
+    // Se inicializa el contenido de la tabla.
+    PRODUCTOS.innerHTML = '';
+    RECORDS.textContent = '';
+    // Se verifica la acción a realizar.
+    (form) ? action = 'search' : action = 'readAll';
+    // Petición para obtener los registros disponibles.
+    const JSON = await dataFetch(PRODUCTO_API, action, form);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se recorre el conjunto de registros fila por fila.
         JSON.dataset.forEach(row => {
-            // Se crean y concatenan las tarjetas con los datos de cada producto.
+            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
             PRODUCTOS.innerHTML += `
-                <div class="col s12 m6 l4">
-                    <div class="card hoverable">
-                        <div class="card-image">
-                            <img src="${SERVER_URL}images/productos/${row.imagen_producto}" class="materialboxed">
-                            <a href="detail.html?id=${row.id_producto}" class="btn-floating halfway-fab waves-effect red tooltipped" data-tooltip="Ver detalle">
-                                <i class="material-icons">more_horiz</i>
-                            </a>
-                        </div>
-                        <div class="card-content">
-                            <span class="card-title">${row.nombre_producto}</span>
-                            <p>Precio(US$) ${row.precio_producto}</p>
-                        </div>
+         
+            <div class="col s10 m3">
+              <div class="card">
+                <div class="card-image waves-effect waves-block waves-light">
+                   <img src="${SERVER_URL}images/products/${row.foto}" class="activator" >  
+                </div>
+                <div class="card-content">
+                  <h3 class="card-title activator grey-text text-darken-4">${row.nombre_producto}</h3>
+                    <div class="modal-footerD">
+                    <span>Codigo: ${row.codigo_producto}</span>
+                    <h5>$ ${row.precio_producto}</h5>
+                    </div>
+                    <div class ="card-action">
+                        <button onclick="openUpdate(${row.id_producto})" class="btn blue tooltipped" data-tooltip="Actualizar">
+                        <i class="material-icons">mode_edit</i>
+                        </button>
+                        <button onclick="openDelete(${row.id_producto})" class="btn red tooltipped" data-tooltip="Eliminar">
+                                <i class="material-icons">delete</i>
+                        </button>
                     </div>
                 </div>
+              </div>
+            </div>
+       
             `;
         });
-        // Se asigna como título la categoría de los productos.
-        TITULO.textContent = PARAMS.get('nombre');
-        // Se inicializa el componente Material Box para que funcione el efecto Lightbox.
-        M.Materialbox.init(document.querySelectorAll('.materialboxed'));
         // Se inicializa el componente Tooltip para que funcionen las sugerencias textuales.
         M.Tooltip.init(document.querySelectorAll('.tooltipped'));
-    }else{
-        // Se presenta un mensaje de error cuando no existen datos para mostrar.
-        TITULO.textContent = JSON.exception;
+        // Se muestra un mensaje de acuerdo con el resultado.
+        RECORDS.textContent = JSON.message;
+    } else {
+        sweetAlert(4, JSON.exception, true);
     }
-});
+}
