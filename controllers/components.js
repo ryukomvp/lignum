70a8414554ee28868264f6bb28ea08c432f8258c
AlecLarios2 <20200332@ricaldone.edu.sv>
2023-07-11 11:13:21 -0600

    probando algunas cosas

--- controllers/components.js
+++ controllers/components.js
@@ -214,7 +214,7 @@ function pieGraph(canvas, legends, values, title) {
     const context = document.getElementById(canvas).getContext('2d');
     // Se crea una instancia para generar el gráfico con los datos recibidos. Requiere la librería chart.js para funcionar.
     const chart = new Chart(context, {
-        type: 'pie',
+        type: 'doughnut',
         data: {
             labels: legends,
             datasets: [{
