c4fd7393ae020f236dd5889933ac25672e6635c4
AlecLarios2 <20200332@ricaldone.edu.sv>
2023-07-16 22:44:13 -0600

    ahora si

--- controllers/dashboard/main.js
+++ controllers/dashboard/main.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     `;
 });
 
-async function graficaPie() {
+async function graficaBar() {
     // Petición para obtener los datos del gráfico.
     const DATA = await dataFetch(PRODUCTO_API, 'productosMaterial');
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
@@ -29,5 +29,27 @@ async function graficaPie() {
     } else {
         document.getElementById('chart1').remove();
         console.log(DATA.exception);
-    } 
+    }
+}
+
+async function graficaPie() {
+    // Petición para obtener los datos del gráfico.
+    const DATA = await dataFetch(PRODUCTO_API, 'productosMaterial');
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
+    if (DATA.status) {
+        // Se declaran los arreglos para guardar los datos a graficar.
+        let proveedores = [];
+        let cantidades = [];
+        // Se recorre el conjunto de registros fila por fila a través del objeto row.
+        DATA.dataset.forEach(row => {
+            // Se agregan los datos a los arreglos.
+            materiales.push(row.proveedor);
+            cantidades.push(row.cantidad)
+        });
+        // Llamada a la función que genera y muestra un gráfico de barras. Se encuentra en el archivo components.js
+        barGraph('chart1', proveedores, cantidades, 'Cantidad de productos', 'Cantidad de productos por proveedor');
+    } else {
+        document.getElementById('chart1').remove();
+        console.log(DATA.exception);
+    }
 }
