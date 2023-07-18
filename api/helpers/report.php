86f62dc576074e307d8899954a7aa2a2de70b77e
ryukomvp <daniel123hernandez15@gmail.com>
2023-07-18 15:16:38 -0600

    Decoración de los reportes

--- api/helpers/report.php
+++ api/helpers/report.php
@@ -58,7 +58,7 @@ class Report extends FPDF
     public function header()
     {
         // Se establece el logo.
-        $this->image('../../images/1.png', 15, 15, 20);
+        $this->image('../../images/logo.png', 15, 15, 20);
         // Se ubica el título.
         $this->cell(20);
         $this->setFont('Arial', 'B', 15);
