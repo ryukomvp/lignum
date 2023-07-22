224a4724f88387280cc4e5497ad296c0afa684a7
ryukomvp <daniel123hernandez15@gmail.com>
2023-07-21 23:03:45 -0600

    Corrección a ruta en archivo de components

--- controllers/components.js
+++ controllers/components.js
@@ -3,7 +3,7 @@
 */
 
 // Constante para establecer la ruta del servidor.
-const SERVER_URL = 'http://localhost/tienda_online/api/';
+const SERVER_URL = 'http://localhost/lignum/api/';
 
 /*
 *   Función para mostrar un mensaje de confirmación. Requiere la librería sweetalert para funcionar.
