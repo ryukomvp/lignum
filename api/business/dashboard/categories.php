a75dea140c502b875c523991a02c604796cc9d10
ryukomvp <daniel123hernandez15@gmail.com>
2023-07-24 08:10:37 -0600

    Reporte parametrizados para ver los productos de una categpría

--- api/business/dashboard/categories.php
+++ api/business/dashboard/categories.php
@@ -16,7 +16,6 @@ if (isset($_GET['action'])) {
             case 'readAll':
                 if ($result['dataset'] = $category->readAll()) {
                     $result['status'] = 1;
-                    $result['message'] = 'Existen '.count($result['dataset']).' registros';
                 } elseif (Database::getException()) {
                     $result['exception'] = Database::getException();
                 } else {
@@ -30,7 +29,6 @@ if (isset($_GET['action'])) {
                     $result['dataset'] = $category->readAll();
                 } elseif ($result['dataset'] = $category->searchRows($_POST['search'])) {
                     $result['status'] = 1;
-                    $result['message'] = 'Existen '.count($result['dataset']).' coincidencias';
                 } elseif (Database::getException()) {
                     $result['exception'] = Database::getException();
                 } else {
